19dd975db0d589a322d87e3cc7dd9084f9201c3c
cna <cna.so@outlook.com>
2023-06-13 17:02:08 +0330

    015 -- remove type any

--- controllers/authorization/jwt/getValuesFromJwt.go
+++ controllers/authorization/jwt/getValuesFromJwt.go
@@ -1,15 +1,16 @@
 package jwt
 
 import (
+	"fmt"
 	"github.com/golang-jwt/jwt/v5"
 )
 
-func GetValueFromJWT(token string, keys []string) (map[string]any, error) {
-	values := make(map[string]any)
+func GetValueFromJWT(token string, keys []string) (map[string]string, error) {
+	values := make(map[string]string)
 	if parsedToken, err := ParseToken(token); err == nil {
 		if claims, ok := parsedToken.Claims.(jwt.MapClaims); ok && parsedToken.Valid {
 			for _, key := range keys {
-				values[key] = claims[key]
+				values[key] = fmt.Sprintf("%s", claims[key])
 			}
 		}
 	}
