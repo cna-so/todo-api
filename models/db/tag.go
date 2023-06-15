063068e7ecac12b09220fb8d570e9458ef8c0fe7
cna <cna.so@outlook.com>
2023-06-15 15:14:10 +0330

    022 -- hide some columns on unmarshal

--- models/db/tag.go
+++ models/db/tag.go
@@ -5,6 +5,6 @@ type Tag struct {
 	Title       string `json:"title" gorm:"not null"`
 	Description string `json:"description"`
 	Color       string `json:"color"`
-	UserID      string
-	Todos       []*Tag `gorm:"many2many:todo_tags"`
+	UserID      string `json:"-"`
+	Todos       []*Tag `gorm:"many2many:todo_tags" json:"-"`
 }
